0ddf909381e5fb9c6a029045ecb8420af62bf952
Nikolai Janakiev <nikolai.janakiev@gmail.com>
2023-03-07 11:29:41 +0100

    Refactor, add users, and add image model with upload in admin

---
+++ images/admin.py
@@ -0,0 +1,26 @@
+from django.contrib import admin
+from .models import Image
+from django.contrib.auth import get_user_model
+
+
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ['name', 'author', 'created_at', 'updated_at', 'image']
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if self.obj:
+            kwargs["initial"] = self.obj.author_id
+            kwargs["queryset"] = get_user_model().objects.filter(id=self.obj.author_id)
+        else:
+            kwargs["initial"] = request.user.id
+            kwargs["queryset"] = get_user_model().objects.filter(id=request.user.id)
+        kwargs["disabled"] = True
+        return super(ImageAdmin, self).formfield_for_foreignkey(
+            db_field, request, **kwargs
+        )
+
+    def get_form(self, request, obj=None, **kwargs):
+        self.obj = obj
+        return super(ImageAdmin, self).get_form(request, obj, **kwargs)
+
+# Register your models here.
+admin.site.register(Image, ImageAdmin)
---
+++ images/models.py
@@ -0,0 +1,35 @@
+from django.conf import settings
+from django.db import models
+from django.utils import timezone
+from django.core.exceptions import ValidationError
+from django.core.validators import FileExtensionValidator
+
+
+def validate_file_size(obj):
+    if obj.size > settings.MAX_FILESIZE:
+        return obj
+    else:
+        return ValidationError("The File is too large")
+
+
+class Image(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(default=timezone.now, editable=True)
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    #file = models.FileField(
+    image = models.ImageField(
+        #blank=True,
+        #null=True,
+        #storage=SFTPStorage(),
+        validators=[
+            validate_file_size,
+            FileExtensionValidator(
+                allowed_extensions=['jpg', 'jpeg', 'png']
+            )
+        ]
+    )
